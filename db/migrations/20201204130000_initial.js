1264aa3004820af186317e9fbc657ae36640c4ac
4lp <alptraq@gmail.com>
2020-12-08 09:11:33 -0600

    Finish initial models and db config

--- db/migrations/20201204130000_initial.js
+++ db/migrations/20201204130000_initial.js
@@ -1,39 +1,43 @@
 exports.up = function(knex, Promise) {
   return knex.schema
-  .createTable('users', function(table) {
-    table.increments();
-    table.string('email').notNullable();
-    table.string('password').notNullable();
-    table.string('username').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
-  })
-  .createTable('shows', function(table) {
-    table.increments();
-    table.string('name').notNullable();
-    table.string('description').notNullable();
-    table.string('embed_link').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
-  })
-  .createTable('products', function(table) {
-    table.increments();
-    table.string('name').notNullable();
-    table.string('description').notNullable();
-    table.string('image_link').notNullable();
-    table.string('creator').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
-  })
-  .createTable('posts', function(table) {
-    table.increments();
-    table.string('content').notNullable();
-    table.string('author_id').notNullable().references('id').inTable('users');
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
-  })
+    .createTable('users', function(table) {
+      table.increments('id');
+      table.string('email').notNullable();
+      table.string('password').notNullable();
+      table.string('username').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
+    .createTable('shows', function(table) {
+      table.increments('id');
+      table.string('name').notNullable();
+      table.text('description').notNullable();
+      table.string('embed_link').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
+    .createTable('products', function(table) {
+      table.increments('id');
+      table.string('name').notNullable();
+      table.text('description').notNullable();
+      table.string('image_link').notNullable();
+      table.string('creator').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
+    .createTable('posts', function(table) {
+      table.increments('id');
+      table.text('content').notNullable();
+      table.integer('author_id').notNullable().references('id').inTable('users');
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
+    });
 }
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return knex.schema
+    .dropTable('posts')
+    .dropTable('products')
+    .dropTable('shows')
+    .dropTable('users');
 }
