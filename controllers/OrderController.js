e6da1f95a6acb52e4fe2708b80595355e164a18f
4lp <alptraq@gmail.com>
2021-01-04 15:38:19 -0600

    Add nodemailer, email orders when submitted

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,4 +1,16 @@
 const Order = require('../models/Order');
+const secrets = require('../secrets.json');
+const nodemailer = require("nodemailer");
+const smtpTransport = require('nodemailer-smtp-transport');
+
+const transporter = nodemailer.createTransport(smtpTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: secrets.gmail.username,
+    pass: secrets.gmail.password
+  }
+}));
 
 const OrderController = {
   async list (req, res, next) {
@@ -32,8 +44,25 @@ const OrderController = {
         .insert(req.body)
         .returning('*')
         .catch(error => next(error));
-      res.status(200).json({ order: order });
+
+      const mailOptions = {
+        from: req.body.email,
+        to: secrets.gmail.to_address,
+        subject: "New Order",
+        text: `${req.body.name} has submitted a new order.
+        
+        ${req.body.order}`
+      }
+      transporter.sendMail(mailOptions, function(error, response){
+        if(error) {
+          console.log(error);
+          res.status(500).json({ error: "Error inserting order" });
+        } else {
+          res.status(200).json({ order: order });
+        }
+      });
     } catch (error) {
+      console.log(error);
       res.status(500).json({ error: "Error inserting order" });
     }
   },
